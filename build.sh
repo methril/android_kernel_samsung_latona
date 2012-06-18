28b3eafc34fe7dfb60c9c7c87b8310757d86750e
Mark Kennard <komcomputers@gmail.com>
2012-06-18 16:17:07 +1200

    build: Updated build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ TOOLCHAIN=/usr/arm-toolchain/bin/arm-eabi-
 ARCH=arm
 MKBOOTIMG=./mkbootimg
 RAMDISK=~/devel/android/cm9/out/target/product/galaxysl/bootstrap.cpio.gz
+BOOTIMAGE=normalboot.img
 
 if [ -z $1 ]; then
 	if [ -z $KBUILD_BUILD_VERSION ]; then
@@ -44,10 +45,14 @@ fi
 
 make -j2 CROSS_COMPILE=$TOOLCHAIN ARCH=$ARCH
 
+
 if test -f arch/arm/boot/zImage; then
-	echo "Tarballing the kernel"
-	$MKBOOTIMG --kernel arch/arm/boot/zImage --ramdisk $RAMDISK --pagesize 1000 -o normalboot.img
-	tar cf $KBUILD_BUILD_VERSION-boot.tar normalboot.img
+	TARBALL=$KBUILD_BUILD_VERSION-boot.tar
+
+	echo "  BOOTIMG $BOOTIMAGE using ramdisk ($RAMDISK)"
+	$MKBOOTIMG --kernel arch/arm/boot/zImage --ramdisk $RAMDISK --pagesize 1000 -o $BOOTIMAGE
+	echo "  TAR     $TARBALL"
+	tar cf $TARBALL $BOOTIMAGE
 else
 	echo "Will not tarball as make didn't produce zImage"
 fi
